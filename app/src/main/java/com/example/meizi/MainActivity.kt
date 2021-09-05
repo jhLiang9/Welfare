c0c37c286fef82bba69ef83ba4f6731cc5204ce2
JH <jhliang9@gmail.com>
2021-09-05 23:54:12 +0800

    Init

---
+++ app/src/main/java/com/example/meizi/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.meizi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//TODO Retrofit ViewPager  存储权限 保存图片 Glide
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
